5b51cc6d0af25171ead730f0f0a4ed40cf2f36e4
ndoxx <bg.physique@gmail.com>
2019-10-03 23:59:10 +0200

    Working on build script

---
+++ scripts/setup.sh
@@ -0,0 +1,49 @@
+
+# Jump to root folder
+cd ..
+
+# Install deps
+sudo apt-get install xorg-dev libxrandr-dev libxinerama-dev libxcursor-dev 
+
+# Update submodules to their last commit
+git submodule update --init
+git submodule foreach "git checkout master"
+
+# Use ImGUI docking branch
+cd source/vendor/imgui
+git checkout docking
+cd ../../..
+
+# Generate GLAD loader
+cd source/vendor/glad
+python -m glad --generator=c --spec=gl --profile=core --api="gl=4.6" --extensions=../../../scripts/glad_extensions.txt --out-path=.
+cd ../../..
+
+# Build deps
+mkdir build
+cd build
+cmake ..
+# GLAD
+make glad
+# GLFW
+cd ../source/vendor/glfw
+mkdir build
+cd build
+cmake ..
+make
+cp src/libglfw3.a ../../../../lib/
+cd ../../../../build
+# Freetype
+cd ../source/vendor/freetype
+./autogen.sh
+./configure --without-harfbuzz CXXFLAGS=-fPIC CFLAGS=-fPIC LDFLAGS=-fPIC CPPFLAGS=-fPIC
+make
+cp objs/.libs/libfreetype.a ../../../lib/
+cp objs/.libs/libfreetype.so* ../../../lib/
+cd ../../../../build
+# Zlib
+cd ../source/vendor/zlib
+./configure
+make
+cp libz.a ../../../lib/
+cd ../../../../build
