829f9fa64e6fd6ca3e70720aed7db21d614b59d2
ndoxx <bg.physique@gmail.com>
2020-04-29 21:32:36 +0200

    Integrated aiekick's ImGuiFileDialog for use in material authoring widget

--- source/Applications/Editor/source/widget/widget_material_authoring.cpp
+++ source/Applications/Editor/source/widget/widget_material_authoring.cpp
@@ -1,8 +1,10 @@
 #include "widget/widget_material_authoring.h"
 #include "asset/asset_manager.h"
+#include "render/renderer.h"
+#include "filesystem/filesystem.h"
 #include "imgui.h"
 #include "imgui/font_awesome.h"
-#include "render/renderer.h"
+#include "ImGuiFileDialog/ImGuiFileDialog.h"
 
 using namespace erwin;
 
@@ -32,6 +34,8 @@ void MaterialAuthoringWidget::on_imgui_render()
 {
     // Restrict to opaque PBR materials for now
     // * For each possible texture map
+    bool show_open_dialog = false;
+    static TMEnum selected_tm = 0;
     void* checkerboard_native = Renderer::get_native_texture_handle(checkerboard_tex_);
     for(TMEnum tm = 0; tm < TextureMapType::TM_COUNT; ++tm)
     {
@@ -50,12 +54,14 @@ void MaterialAuthoringWidget::on_imgui_render()
         	return;
 
         // Context menu
-        bool show_open_dialog = false;
         ImGui::PushID(int(ImGui::GetID(reinterpret_cast<void*>(intptr_t(tm)))));
         if(ImGui::BeginPopupContextItem("##TM_CONTEXT"))
         {
             if(ImGui::Selectable("Load"))
+            {
                 show_open_dialog = true;
+                selected_tm = tm;
+            }
 
             if(has_map)
             {
@@ -72,13 +78,29 @@ void MaterialAuthoringWidget::on_imgui_render()
             ImGui::EndPopup();
         }
         ImGui::PopID();
-
-        if(show_open_dialog)
-        {
-        	DLOG("editor",1) << "Loading texture map: " << s_texture_map_names[tm] << std::endl;
-        }
-
         ImGui::Separator();
+    }
+
+    if(show_open_dialog)
+    {
+    	// const std::string& dir = (filesystem::get_asset_dir() / "textures/map/upack/").string();
+    	igfd::ImGuiFileDialog::Instance()->OpenModal("ChooseFileDlgKey", "Choose File", ".png", ".");
+    }
+    if(igfd::ImGuiFileDialog::Instance()->FileDialog("ChooseFileDlgKey"))
+    {
+        // action if OK
+        if(igfd::ImGuiFileDialog::Instance()->IsOk == true)
+        {
+            std::string filepath = igfd::ImGuiFileDialog::Instance()->GetFilepathName();
+    		DLOG("editor",1) << "Loading texture map: " << s_texture_map_names[selected_tm] << std::endl;
+    		DLOGI << "Path: " << WCC('p') << filepath << std::endl;
+    		Texture2DDescriptor desc;
+    		TextureHandle tex = AssetManager::load_image(filepath, desc);
+    		current_composition_.textures[size_t(selected_tm)] = tex;
+    		current_composition_.set_map(TextureMapType(selected_tm));
+        }
+        // close
+        igfd::ImGuiFileDialog::Instance()->CloseDialog("ChooseFileDlgKey");
     }
 }
 
