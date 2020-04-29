e0952072bc60a9957ee80948bdf827cd138287d1
ndoxx <bg.physique@gmail.com>
2020-04-29 07:48:20 +0200

    backup

--- source/Applications/Editor/source/widget/widget_material_authoring.cpp
+++ source/Applications/Editor/source/widget/widget_material_authoring.cpp
@@ -1,39 +1,85 @@
 #include "widget/widget_material_authoring.h"
-#include "render/renderer.h"
 #include "asset/asset_manager.h"
 #include "imgui.h"
+#include "imgui/font_awesome.h"
+#include "render/renderer.h"
 
 using namespace erwin;
 
 namespace editor
 {
 
+static std::vector<std::string> s_texture_map_names = {
+    "Albedo",            // TM_ALBEDO
+    "Normal",            // TM_NORMAL
+    "Depth",             // TM_DEPTH
+    "Metal",             // TM_METAL
+    "Ambient Occlusion", // TM_AO
+    "Roughness",         // TM_ROUGHNESS
+    "Emissivity",        // TM_EMISSIVITY
+};
 
-MaterialAuthoringWidget::MaterialAuthoringWidget():
-Widget("Material authoring", true)
+MaterialAuthoringWidget::MaterialAuthoringWidget() : Widget("Material authoring", true)
 {
-	checkerboard_tex_ = AssetManager::create_debug_texture("checkerboard"_h, 64);
+    checkerboard_tex_ = AssetManager::create_debug_texture("checkerboard"_h, 64);
 }
 
-MaterialAuthoringWidget::~MaterialAuthoringWidget()
-{
-	
-}
+MaterialAuthoringWidget::~MaterialAuthoringWidget() {}
+
+static constexpr size_t k_image_size = 115;
 
 void MaterialAuthoringWidget::on_imgui_render()
 {
-	// Restrict to opaque PBR materials for now
-	// * For each possible texture map
-
-	// * Display a clickable image of the texture map
-	// Default to checkerboard pattern if no texture map is loaded
+    // Restrict to opaque PBR materials for now
+    // * For each possible texture map
     void* checkerboard_native = Renderer::get_native_texture_handle(checkerboard_tex_);
-    if(checkerboard_native)
+    for(TMEnum tm = 0; tm < TextureMapType::TM_COUNT; ++tm)
     {
-		ImGui::Image(checkerboard_native, ImVec2(64, 64));
-	}
-	// Display currently bound texture map otherwise
-}
+    	bool has_map = current_composition_.has_map(TextureMapType(tm));
+        ImGui::TextColored({0.5f, 0.7f, 0.f, 1.f}, "%s %s", W_ICON(PICTURE_O), s_texture_map_names[tm].c_str());
 
+        // * Display a clickable image of the texture map
+        // Display currently bound texture map
+        // Default to checkerboard pattern if no texture map is loaded
+	    TextureHandle tex = current_composition_.textures[size_t(tm)];
+        void* image_native = has_map ? Renderer::get_native_texture_handle(tex) : checkerboard_native;
+
+        if(image_native)
+            ImGui::Image(image_native, ImVec2(k_image_size, k_image_size));
+        else
+        	return;
+
+        // Context menu
+        bool show_open_dialog = false;
+        ImGui::PushID(int(ImGui::GetID(reinterpret_cast<void*>(intptr_t(tm)))));
+        if(ImGui::BeginPopupContextItem("##TM_CONTEXT"))
+        {
+            if(ImGui::Selectable("Load"))
+                show_open_dialog = true;
+
+            if(has_map)
+            {
+	            if(ImGui::Selectable("Clear"))
+	            {
+	                TextureHandle tex = current_composition_.textures[size_t(tm)];
+	                if(tex.is_valid())
+	                {
+	                	Renderer::destroy(tex);
+	                	current_composition_.clear_map(TextureMapType(tm));
+	                }
+	            }
+            }
+            ImGui::EndPopup();
+        }
+        ImGui::PopID();
+
+        if(show_open_dialog)
+        {
+        	DLOG("editor",1) << "Loading texture map: " << s_texture_map_names[tm] << std::endl;
+        }
+
+        ImGui::Separator();
+    }
+}
 
 } // namespace editor
--- source/Applications/Editor/source/widget/widget_material_authoring.h
+++ source/Applications/Editor/source/widget/widget_material_authoring.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "widget/widget.h"
+#include "asset/material.h"
 #include "render/handles.h"
+#include "render/texture_common.h"
 
 namespace editor
 {
@@ -17,6 +19,18 @@ protected:
 
 private:
 	erwin::TextureHandle checkerboard_tex_;
+
+	struct MaterialComposition
+	{
+		TMEnum texture_maps = TMF_NONE;
+		erwin::TextureGroup textures;
+
+        inline bool has_map(TextureMapType map_type)   { return bool(texture_maps & (1 << map_type)); }
+        inline void set_map(TextureMapType map_type)   { texture_maps |= (1 << map_type); }
+        inline void clear_map(TextureMapType map_type) { texture_maps &= ~(1 << map_type); textures[size_t(map_type)] = {}; }
+    };
+
+	MaterialComposition current_composition_;
 };
 
 } // namespace editor
