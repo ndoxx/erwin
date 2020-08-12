ec363202ebcee2fca7e3cb6ce6b79cdbcfe490f3
ndoxx <bg.physique@gmail.com>
2020-08-12 23:04:19 +0200

    Wrote a more secure sparse pool for handles

--- source/Erwin/utils/sparse_set.hpp
+++ source/Erwin/utils/sparse_set.hpp
@@ -2,9 +2,9 @@
 
 #include <array>
 #include <cassert>
+#include <numeric>
 #include <type_traits>
 #include <vector>
-#include <numeric>
 
 namespace erwin
 {
@@ -168,6 +168,82 @@ public:
         T index = sparse[handle];
         T temp = dense[--size_];
         dense[size_] = handle;
+        sparse[temp] = index;
+        dense[index] = temp;
+    }
+};
+
+template <typename UIntT, typename = std::enable_if_t<std::is_unsigned_v<UIntT>>>
+constexpr UIntT make_mask(const UIntT shift_pos)
+{
+    UIntT result = 0;
+
+    for(UIntT ii = shift_pos; ii < sizeof(UIntT) * UIntT(8); ++ii)
+        result |= UIntT(1u) << ii;
+
+    return result;
+}
+
+template <typename T, size_t SIZE, size_t GUARD_BITS> class SecureSparsePool
+{
+    static_assert(std::is_unsigned_v<T>, "SecureSparsePool can only contain unsigned integers");
+
+private:
+    using Array = std::array<T, SIZE>;
+    Array dense;
+    Array sparse; // Map of elements to dense set indices
+
+    size_t size_ = 0; // Element count
+
+public:
+    using iterator = typename Array::const_iterator;
+    using const_iterator = typename Array::const_iterator;
+    static constexpr T k_guard_shift = sizeof(T) * T(8u) - GUARD_BITS;
+    static constexpr T k_guard_mask = make_mask<T>(k_guard_shift);
+    static constexpr T k_handle_mask = ~k_guard_mask;
+
+    SecureSparsePool() : size_(0) { clear(); }
+
+    inline iterator begin() { return dense.begin(); }
+    inline iterator end() { return dense.begin() + size_; }
+    inline const_iterator begin() const { return dense.begin(); }
+    inline const_iterator end() const { return dense.begin() + size_; }
+
+    inline size_t size() const { return size_; }
+    inline bool empty() const { return size_ == 0; }
+    inline bool is_valid(const T& val) const
+    {
+        T unguarded = val & k_handle_mask;
+        return unguarded < SIZE && sparse[unguarded] < size_ && dense[sparse[unguarded]] == val;
+    }
+
+    void clear()
+    {
+        size_ = 0;
+        std::iota(dense.begin(), dense.end(), 0);
+    }
+
+    T acquire()
+    {
+        assert(size_ + 1 < SIZE && "Exceeding SecureSparsePool capacity");
+
+        T index = T(size_++);
+        T handle = dense[index];
+        T unguarded = handle & k_handle_mask;
+        sparse[unguarded] = index;
+
+        return handle;
+    }
+
+    void release(T handle)
+    {
+        assert(is_valid(handle) && "Cannot release invalid handle");
+
+        T unguarded = handle & k_handle_mask;
+        T new_guard = (handle & k_guard_mask) + (T(1u) << k_guard_shift);
+        T index = sparse[unguarded];
+        T temp = dense[--size_] & k_handle_mask;
+        dense[size_] = unguarded | new_guard;
         sparse[temp] = index;
         dense[index] = temp;
     }
--- source/tests/nuclear.cpp
+++ source/tests/nuclear.cpp
@@ -20,30 +20,32 @@
 
 #include "core/clock.hpp"
 #include "ctti/type_id.hpp"
-#include "debug/logger.h"
-#include "debug/logger_sink.h"
-#include "debug/logger_thread.h"
 #include "glm/glm.hpp"
 #include "glm/gtx/string_cast.hpp"
+#include "kibble/logger/logger.h"
+#include "kibble/logger/logger_sink.h"
+#include "kibble/logger/logger_thread.h"
 #include "memory/memory.hpp"
 #include "utils/random_operations.hpp"
 #include "utils/sparse_set.hpp"
 
-#include "entity/component/hierarchy.h"
-#include "entity/component/transform.h"
-#include "entity/component/camera.h"
-#include "entity/component/bounding_box.h"
-#include "entity/component/mesh.h"
 #include "entity/component/PBR_material.h"
-#include "entity/component/dirlight_material.h"
-#include "entity/component/light.h"
+#include "entity/component/bounding_box.h"
+#include "entity/component/camera.h"
 #include "entity/component/description.h"
+#include "entity/component/dirlight_material.h"
+#include "entity/component/hierarchy.h"
+#include "entity/component/light.h"
+#include "entity/component/mesh.h"
+#include "entity/component/script.h"
+#include "entity/component/transform.h"
 
 #include "entt/entt.hpp"
 
 #include "filesystem/wpath.h"
 
 using namespace erwin;
+using namespace kb;
 
 // TMP
 class Scene;
@@ -52,48 +54,62 @@ template <> void erwin::inspector_GUI<ComponentCamera3D>(ComponentCamera3D&, Ent
 template <> void erwin::inspector_GUI<ComponentOBB>(ComponentOBB&, EntityID, Scene&) {}
 template <> void erwin::inspector_GUI<ComponentMesh>(ComponentMesh&, EntityID, Scene&) {}
 template <> void erwin::inspector_GUI<ComponentPBRMaterial>(ComponentPBRMaterial&, EntityID, Scene&) {}
-template <> void erwin::inspector_GUI<ComponentDirectionalLightMaterial>(ComponentDirectionalLightMaterial&, EntityID, Scene&) {}
+template <>
+void erwin::inspector_GUI<ComponentDirectionalLightMaterial>(ComponentDirectionalLightMaterial&, EntityID, Scene&)
+{}
 template <> void erwin::inspector_GUI<ComponentDirectionalLight>(ComponentDirectionalLight&, EntityID, Scene&) {}
 template <> void erwin::inspector_GUI<ComponentScript>(ComponentScript&, EntityID, Scene&) {}
 
 void init_logger()
 {
-    WLOGGER(create_channel("memory", 3));
-    WLOGGER(create_channel("thread", 3));
-    WLOGGER(create_channel("nuclear", 3));
-    WLOGGER(create_channel("entity", 3));
-    WLOGGER(create_channel("config", 3));
-    WLOGGER(create_channel("render", 3));
-    WLOGGER(create_channel("asset", 3));
-    WLOGGER(attach_all("console_sink", std::make_unique<dbg::ConsoleSink>()));
-    WLOGGER(set_single_threaded(true));
-    WLOGGER(set_backtrace_on_error(false));
-    WLOGGER(spawn());
-    WLOGGER(sync());
+    KLOGGER_START();
+    KLOGGER(create_channel("memory", 3));
+    KLOGGER(create_channel("thread", 3));
+    KLOGGER(create_channel("nuclear", 3));
+    KLOGGER(create_channel("entity", 3));
+    KLOGGER(create_channel("config", 3));
+    KLOGGER(create_channel("render", 3));
+    KLOGGER(create_channel("asset", 3));
+    KLOGGER(attach_all("console_sink", std::make_unique<klog::ConsoleSink>()));
+    KLOGGER(set_single_threaded(true));
+    KLOGGER(set_backtrace_on_error(false));
+    KLOGGER(spawn());
+    KLOGGER(sync());
 
     DLOGN("nuclear") << "Nuclear test" << std::endl;
-}
-
-void show(const WPath& p)
-{
-    BANG();
-    DLOG("nuclear",1) << p << std::endl;
-    DLOG("nuclear",1) << p.absolute() << std::endl;
-    DLOG("nuclear",1) << p.relative() << std::endl;
 }
 
 int main(int argc, char** argv)
 {
     init_logger();
 
-    WPath::set_root_directory("/home/ndx/dev/Erwin");
-    WPath::set_resource_directory("/home/ndx/erwin_workspace/test_scene/assets");
+    SecureSparsePool<uint32_t, 64, 16> handle_pool;
 
-    auto p = "res://textures/materials/dirtyWickerWeave.tom"_wp;
-    show(p);
+    DLOG("nuclear",1) << std::bitset<32>(handle_pool.k_guard_mask) << std::endl;
+    DLOG("nuclear",1) << std::bitset<32>(handle_pool.k_handle_mask) << std::endl;
 
-    auto p2 = "/home/ndx/erwin_workspace/test_scene/assets/textures/materials/dirtyWickerWeave.tom"_wp;
-    show(p2);
+    DLOGN("nuclear") << "Testing" << std::endl;
+    for(size_t ii=0; ii<8; ++ii)
+    {
+        auto h0 = handle_pool.acquire();
+        auto h1 = handle_pool.acquire();
+        DLOG("nuclear",1) << std::bitset<32>(h0) << std::endl;
+        DLOG("nuclear",1) << std::bitset<32>(h1) << std::endl;
+        handle_pool.release(h0);
+    }
+    auto h0 = handle_pool.acquire();
+
+    DLOG("nuclear",1) << std::boolalpha << handle_pool.is_valid(0) << std::endl;
+    DLOG("nuclear",1) << std::boolalpha << handle_pool.is_valid(1) << std::endl;
+    DLOG("nuclear",1) << std::boolalpha << handle_pool.is_valid(18) << std::endl;
+    DLOG("nuclear",1) << std::boolalpha << handle_pool.is_valid(h0) << std::endl;
+    DLOG("nuclear",1) << std::boolalpha << handle_pool.is_valid(0b00000000000010000000000000000000) << std::endl;
+    
+    BANG();
+    handle_pool.release(1);
+    DLOG("nuclear",1) << std::boolalpha << handle_pool.is_valid(1) << std::endl;
+    auto h1 = handle_pool.acquire();
+    DLOG("nuclear",1) << std::bitset<32>(h1) << std::endl;
 
     return 0;
 }
