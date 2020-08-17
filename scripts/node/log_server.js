16e2c14c31d98b9f3f1fa98db1fd71aa6e45cb2a
ndoxx <bg.physique@gmail.com>
2020-08-17 02:23:14 +0200

    Node server also serves webpage

--- scripts/node/log_server.js
+++ scripts/node/log_server.js
@@ -30,4 +30,24 @@ wss.on('connection', function connection(ws) {
 	};
 });
 
+// HTTP and static file servers
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var nstatic = require('node-static');
+var fileServer = new nstatic.Server('./html');
 
+var server = http.createServer(function(request, response) {
+    request.addListener('end', function () {
+	    fileServer.serve(request, response, function (err, result) {
+	        if (err) { // There was an error serving the file
+	            console.error("Error serving " + request.url + " - " + err.message);
+
+	            // Respond to the client
+	            response.writeHead(err.status, err.headers);
+	            response.end();
+	        }
+	    });
+    }).resume();
+});  
+server.listen(8080);
