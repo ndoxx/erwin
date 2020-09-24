9919cd981cb0889da9990043d6b53388f6972181
ndoxx <bg.physique@gmail.com>
2020-09-24 07:23:08 +0200

    Cleanup

--- scripts/node/log_server.js
+++ scripts/node/log_server.js
@@ -2,34 +2,29 @@
 
 const WebSocket = require('ws');
 const net = require('net');
-const wss = new WebSocket.Server({ port: 8337 });
+const wss = new WebSocket.Server({port : 8337});
 
 const tcp_port = 31337;
 const tcp_host = '127.0.0.1';
 
 var wsock = false;
-wss.on('connection', function connection(ws) {
-	wsock = ws;
-});
+wss.on('connection', function connection(ws) { wsock = ws; });
 
 const tcp_server = net.createServer(onClientConnection);
-tcp_server.listen(tcp_port,tcp_host,function(){
-   console.log(`TCP server started on port ${tcp_port} at ${tcp_host}`); 
-});
+tcp_server.listen(tcp_port, tcp_host,
+                  function() { console.log(`TCP server started on port ${tcp_port} at ${tcp_host}`); });
 
 // TCP Listener
-function onClientConnection(sock){
+function onClientConnection(sock)
+{
     console.log(`${sock.remoteAddress}:${sock.remotePort} Connected`);
-    sock.on('data',function(data){
+    sock.on('data', function(data) {
         console.log(`${sock.remoteAddress}:${sock.remotePort}: ${data}`);
         wsock.send(`${data}`);
     });
-    sock.on('close',function(){
-        console.log(`${sock.remoteAddress}:${sock.remotePort} Terminated the connection`);
-    });
-    sock.on('error',function(error){
-        console.error(`${sock.remoteAddress}:${sock.remotePort} Connection Error ${error}`);
-    });
+    sock.on('close', function() { console.log(`${sock.remoteAddress}:${sock.remotePort} Terminated the connection`); });
+    sock.on('error',
+            function(error) { console.error(`${sock.remoteAddress}:${sock.remotePort} Connection Error ${error}`); });
 };
 
 // HTTP and static file servers
@@ -40,21 +35,26 @@ var nstatic = require('node-static');
 var fileServer = new nstatic.Server('./html');
 
 var server = http.createServer(function(request, response) {
-	// Redirect root path to log.html
-	if(request.url == '/') {
-		fileServer.serveFile('/log.html', 200, {}, request, response);
-		return;
-	}
-    request.addListener('end', function () {
-	    fileServer.serve(request, response, function (err, result) {
-	        if(err) { // There was an error serving the file
-	            console.error("Error serving " + request.url + " - " + err.message);
+    // Redirect root path to log.html
+    if(request.url == '/')
+    {
+        fileServer.serveFile('/log.html', 200, {}, request, response);
+        return;
+    }
+    request
+        .addListener('end',
+                     function() {
+                         fileServer.serve(request, response, function(err, result) {
+                             if(err)
+                             { // There was an error serving the file
+                                 console.error("Error serving " + request.url + " - " + err.message);
 
-	            // Respond to the client
-	            response.writeHead(err.status, err.headers);
-	            response.end();
-	        }
-	    });
-    }).resume();
-});  
-server.listen(8080);
+                                 // Respond to the client
+                                 response.writeHead(err.status, err.headers);
+                                 response.end();
+                             }
+                         });
+                     })
+        .resume();
+});
+server.listen(8095);
